c5d9147716ce69eba676dc900f06e02a251e51d3
KleanthisLiontis <kleanthisliontis@gmail.com>
2023-06-06 19:53:30 +0300

    Updates to qrcode component

--- Interface/src/components/create-qr-code.js
+++ Interface/src/components/create-qr-code.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import { QRCodeSVG } from 'qrcode.react';
-import QrcodeIcon from '@heroicons/react/24/solid/QrcodeIcon';
+//import QrcodeIcon from '@heroicons/react/24/solid/QrcodeIcon';
+import LinkIcon from '@heroicons/react/24/solid/LinkIcon';
 import { Button, SvgIcon } from '@mui/material';
 
 export const CreateQRcodeButton = () => {
   const openQRCodeWindow = () => {
-    const qrCodeUrl = 'https://reactjs.org/'; // Replace with your desired URL
-    const windowFeatures = 'width=400,height=400,scrollbars=yes';
-    window.open(qrCodeUrl, '_blank', windowFeatures);
-  };
-
-  return (
-    <Button
-    startIcon={(
-      <SvgIcon fontSize="small">
-        <QrcodeIcon />
-      </SvgIcon>
-    )}
-    variant="contained"
-      onClick={openQRCodeWindow}
-    >
-      Create QR Code
-    </Button>
-  );
-};
-
-export default CreateQRcodeButton;
+    //const qrCodeUrl = 'https://reactjs.org/';
+    const text = 'Hello, world!';
+     // Calculate the center position of the screen
+     const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+     const screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+     const windowLeft = (screenWidth - 400) / 2;
+     const windowTop = (screenHeight - 400) / 2;
+ 
+     // Create a new document with the string content
+     const documentContent = `
+       <html>
+         <body>
+           <div style="display: flex; align-items: center; justify-content: center; height: 100%;">
+             <h1>${text}</h1>
+           </div>
+         </body>
+       </html>
+     `;
+ 
+     // Create a new window and write the document content
+     const windowObject = window.open('', '_blank', `top=${windowTop},left=${windowLeft},width=400,height=400,scrollbars=yes`);
+     windowObject.document.open();
+     windowObject.document.write(documentContent);
+     windowObject.document.close();
+   };
+ 
+   return (
+     <Button
+       startIcon={(
+         <SvgIcon fontSize="small">
+           <LinkIcon />
+         </SvgIcon>
+       )}
+       variant="contained"
+       onClick={openQRCodeWindow}
+     >
+       Create Supplier Link Code
+     </Button>
+   );
+ };
+ 
+ export default CreateQRcodeButton;
