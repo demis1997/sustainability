e7e57ebe11bd110a3e61281864028d2029c68f1c
demis1997 <demis.konstantinou16@gmail.com>
2023-06-10 15:09:57 +0300

    sceript for running

--- venom/supplychain/scripts/1-deploy-sample.ts
+++ venom/supplychain/scripts/1-deploy-sample.ts
@@ -1,18 +1,20 @@
+import { Address } from "locklift";
 async function main() {
   const signer = (await locklift.keystore.getSigner("0"))!;
-  const { contract: sample, tx } = await locklift.factory.deployContract({
-    contract: "Sample",
+  const { contract: RetailShop, tx } = await locklift.factory.deployContract({
+    contract: "RetailShop",
     publicKey: signer.publicKey,
     initParams: {
       _nonce: locklift.utils.getRandomNonce(),
+      
     },
     constructorParams: {
-      _state: 0,
+
     },
     value: locklift.utils.toNano(3),
   });
 
-  console.log(`Sample deployed at: ${sample.address.toString()}`);
+  console.log(`RetailShops deployed at: ${RetailShop.address.toString()}`);
 }
 
 main()
